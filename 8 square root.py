0a99b2880424468b9c871172bfa1a77056b3f35f
Aakashv34 <aakashv3434@gmail.com>
2024-10-01 11:36:30 +0530

    Add files via upload

---
+++ 8 square root.py
@@ -0,0 +1,6 @@
+length=int(input("enter the length of the square:"))
+breadth=int(input("enter the breadth of the square:"))
+if length==breadth:
+    print("it is a square")
+else:
+    print("its not a square")
